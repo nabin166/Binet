69d1596e1f1d45782745d047896e67887384c67f
nabin166 <nabinaryal830@gmail.com>
2024-09-14 14:46:24 +0545

    Optimize LV1

---
+++ BinetWebsite/Controllers/Ongoingproject.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BinetWebsite.Controllers
+{
+    public class Ongoingproject : Controller
+    {
+        public IActionResult binetGold()
+        {
+            return PartialView();
+        }
+        [HttpGet]
+        public IActionResult nepalMart()
+        {
+            return PartialView("nepalMart");
+        }
+      
+        
+        public IActionResult fenegosida()
+        {
+            return PartialView("fenegosida");
+        }
+        public IActionResult stockManagement()
+        {
+            return PartialView("stockManagement");
+        }
+        public IActionResult accountingSoftware()
+        {
+            return PartialView("accountingSoftware");
+        }
+        public IActionResult numerologyApp()
+        {
+            return PartialView("numerologyApp");
+        }
+        public IActionResult socialNetwork()
+        {
+            return PartialView("socialNetwork");
+        }
+    }
+}
--- BinetWebsite/wwwroot/js/site.js
+++ BinetWebsite/wwwroot/js/site.js
@@ -2,3 +2,82 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    console.log("Hiii")
+    $('#loadmart').click(function () {
+        $.ajax({
+            url: '/Ongoingproject/nepalMart',
+            type: 'GET',
+            success: function (data) {
+                $('#partialContainer').html(data);
+            },
+            error: function (xhr, status, error) {
+                console.error("Error loading partial view:", error);
+            }
+        });
+    });
+
+    $('#loadfenegosida').click(function () {
+        $.ajax({
+            url: '/Ongoingproject/fenegosida',
+            type: 'GET',
+            success: function (data) {
+                $('#partialContainerfen').html(data);
+            },
+            error: function (xhr, status, error) {
+                console.error("Error loading partial view:", error);
+            }
+        });
+    });
+
+    $('#loadstock').click(function () {
+        $.ajax({
+            url: '/Ongoingproject/stockManagement',
+            type: 'GET',
+            success: function (data) {
+                $('#partialContainerstock').html(data);
+            },
+            error: function (xhr, status, error) {
+                console.error("Error loading partial view:", error);
+            }
+        });
+    });
+    $('#loadaccounting').click(function () {
+        $.ajax({
+            url: '/Ongoingproject/accountingSoftware',
+            type: 'GET',
+            success: function (data) {
+                $('#partialContaineraccount').html(data);
+            },
+            error: function (xhr, status, error) {
+                console.error("Error loading partial view:", error);
+            }
+        });
+    });
+    $('#loadAnynomous').click(function () {
+        $.ajax({
+            url: '/Ongoingproject/socialNetwork',
+            type: 'GET',
+            success: function (data) {
+                $('#partialContainersocialnetwork').html(data);
+            },
+            error: function (xhr, status, error) {
+                console.error("Error loading partial view:", error);
+            }
+        });
+    });
+    $('#loadnumerology').click(function () {
+        $.ajax({
+            url: '/Ongoingproject/numerologyApp',
+            type: 'GET',
+            success: function (data) {
+                $('#partialContainernumerology').html(data);
+            },
+            error: function (xhr, status, error) {
+                console.error("Error loading partial view:", error);
+            }
+        });
+    });
+
+});
